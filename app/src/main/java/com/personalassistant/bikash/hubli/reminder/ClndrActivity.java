0c3972da4a4463e6b967c906ce4ce20fa0b542f4
Bikash <bikashkumarsahu9@gmail.com>
2018-04-24 19:10:47 +0530

    Initial commit

---
+++ app/src/main/java/com/personalassistant/bikash/hubli/reminder/ClndrActivity.java
@@ -0,0 +1,15 @@
+package com.personalassistant.bikash.hubli.reminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.personalassistant.bikash.hubli.R;
+
+public class ClndrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clndr);
+    }
+}
